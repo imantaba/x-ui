dd3c05b1c008bc9f2cbeb4d949f89cf7fd45db88
imantaba <itn.taba@gmail.com>
2022-10-31 12:46:41 +0330

    add run x-ui

---
+++ run-x-ui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+$domain = lrs.lrnito.ir
+$email = itn.taba@gmail.com
+# please run as root
+apt update && apt upgrade -y
+apt install curl socat -y
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
+bash <(curl -Ls https://raw.githubusercontent.com/imantaba/x-ui/main/install.sh)
+
+# Fourth question set the port to 54544
